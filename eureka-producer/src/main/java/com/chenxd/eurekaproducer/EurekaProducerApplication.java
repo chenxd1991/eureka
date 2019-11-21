c7c4b9819ec157b095f76be928b3d4dbf9193d8c
xiaodong.chen <xiaodong.chen@huixiaoer.com>
2019-11-21 13:35:26 +0800

    服务初始化

---
+++ eureka-producer/src/main/java/com/chenxd/eurekaproducer/EurekaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.chenxd.eurekaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProducerApplication.class, args);
+    }
+
+}
