5b443640e31e5018ba02d1efb711b925c43e4bc2
chenxd1991 <18246068557@163.com>
2019-11-26 23:46:18 +0800

    增加gateway

---
+++ eureka-gateway/src/main/java/com/chenxd/eurekagateway/EurekaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.chenxd.eurekagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaGatewayApplication.class, args);
+    }
+
+}
---
+++ eureka-gateway/src/test/java/com/chenxd/eurekagateway/EurekaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenxd.eurekagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
